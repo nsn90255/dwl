57743a9a8f3c8b7861b64af2db1916ccc8f5af5d
nsn90255 <nsn90255@gmail.com>
2024-12-21 00:10:01 +0100

    wmenu colour

--- config.def.h
+++ config.def.h
@@ -128,7 +128,7 @@ static const enum libinput_config_tap_button_map button_map = LIBINPUT_CONFIG_TA
 
 /* commands */
 static const char *termcmd[] = { "foot", NULL };
-static const char *menucmd[] = { "wmenu-run", NULL };
+static const char *menucmd[] = { "wmenu-run", "-S", "92b780" };
 static const char *browsercmd[] = { "librewolf", NULL };
 
 static const Key keys[] = {
